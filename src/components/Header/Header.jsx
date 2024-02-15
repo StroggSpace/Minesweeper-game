882cc2008e93828a4323b47989e847d0b9add370
Juriy_Strogg <Strog1998@gmail.com>
2024-02-15 22:14:17 +0500

    valid header link

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,13 @@
+import { Link } from "react-router-dom";
 import Logo from "../../assets/gnome-mines.svg?react";
 export default function Header() {
   return (
     <header className="header">
       <div className="header__logo-container">
-        <a className="header__logo" href="/">
+        <Link to={`/`} className="header__logo">
           <Logo className="header__logo-icon" />
           <h1 className="header__logo-title">Minesweeper</h1>
-        </a>
+        </Link>
       </div>
     </header>
   );
